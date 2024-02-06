f518f573266f00207836c2df9e03b2ea163b172c
Yug2801 <95994453+Yug2801@users.noreply.github.com>
2024-02-06 15:49:07 +0530

    Revert "without backend and without transition"
    This reverts commit 13787ef0b49aa9ff4ff7c40201d58784ef22dd03.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { useState,useEffect } from 'react';
-import Main from './components/Main';
-import Contactus from './components/contact';
-import Header from './components/header';
-import BlogPage from './components/blog';
-import AboutUsPage from './components/Aboutus';
-import {  Route, Routes } from 'react-router-dom';
-import Preloader from './components/preloader';
 
 function App() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []); 
-
   return (
-    <div>
-    {loading ? (
-      <Preloader />
-    ) : (
     <div className="App">
-    <header><Header/></header>
-      <Routes scrollToTop>
-        <Route  path="/" element={<Main/>} />
-        <Route  path="/Cyph3r.git" element={<Main/>} />
-        <Route path='/blog' element={<BlogPage/>}/>
-        <Route path='/contactus' element={<Contactus/>}/>
-        <Route path='/about' element={<AboutUsPage/>}/>
-      </Routes>
-
-    </div>
-    )}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
-export default App;
+}
 
+export default App;
